95658297bca9201ff9c89a6f4bb84451c1777342
Viacheslav Ivanov <viacheslav.ivanov@kofax.com>
2022-09-03 06:55:54 +0200

    Add command files to run
    Also, adds .gitignore

---
+++ Generate.cmd
@@ -0,0 +1,4 @@
+@Cd "Generate\bin\Release\net6.0"
+@Generate ".\..\..\..\..\Results\DataSort-1G.txt"
+@Generate ".\..\..\..\..\Results\DataSort-10G.txt" 100000000
+@Cd ".\..\..\..\..\"
---
+++ Sort.cmd
@@ -0,0 +1,3 @@
+@Cd "Sort\bin\Release\net6.0"
+@Sort ".\..\..\..\..\Results\DataSort-10G.txt"
+@Cd ".\..\..\..\..\"
